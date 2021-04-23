0023cea682d7dbc0a089aa80106ff5f06ab56d96
josue_aguilar <josueaguilarjnai@gmail.com>
2021-04-23 11:51:28 -0500

    Creating main.go

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hi! My name is Josue. Enjoy the exercises.")
+	fmt.Println("If you have any opinion or idea, send me a mail, here: josueaguilarjnai@gmail.com")
+}
